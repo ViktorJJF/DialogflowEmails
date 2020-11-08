ab092540cdbda26e3abce4041b7bcb9b45fb1618
ViktorJJF <vj.jimenez96@gmail.com>
2020-11-07 22:26:23 -0500

    first commit

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
-const express = require("express");
-const app = express();
-const { WebhookClient } = require("dialogflow-fulfillment");
-
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
-
-app.post("/webhook", express.json(), function (req, res) {
-  const agent = new WebhookClient({ request: req, response: res });
-  console.log("Dialogflow Request headers: " + JSON.stringify(req.headers));
-  console.log("Dialogflow Request body: " + JSON.stringify(req.body));
-
-  function welcome(agent) {
-    agent.add(`Welcome to my agent!`);
+'use strict';
+ 
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+const axios=require("axios");
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey("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");
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+	
+  function crearTramite(agent){
+    let email=agent.parameters["email"];
+  	let DNI=agent.parameters["DNI"];
+    let Nombres=agent.parameters["Nombres"];
+    let Apellidos=agent.parameters["Apellidos"];
+    let Archivo=agent.parameters["Archivo"];
+    let NroSeguimiento=Date.now();
+    let Estado="PENDIENTE";
+    axios.post("https://sheet.best/api/sheets/d3dcf808-5f26-448d-ab13-3b23f8a5b08e",{DNI,Nombres,Apellidos,Archivo,NroSeguimiento,Estado});
+    const msg = {
+  to: email, // Change to your recipient
+  from: 'victorjuanjf@jfbots.com',
+  templateId:"d-5d968c5f54fd49a8ad9400be5b05583a",
+  dynamic_template_data:{DNI,Nombres,Apellidos,Archivo,NroSeguimiento}
+				};
+	sgMail.send(msg);
+    
+    agent.add("Tu trámite fue registrado exitosamente ... ");
+    agent.add("Tu número de seguimento del trámite es: "+NroSeguimiento);
   }
-
-  function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
+  
+  async function consultarTramite(agent){
+  	let NroSeguimiento=agent.parameters["NroSeguimiento"];
+    let respuesta=await axios.get("https://sheet.best/api/sheets/d3dcf808-5f26-448d-ab13-3b23f8a5b08e/NroSeguimiento/"+NroSeguimiento);
+  	let tramites=respuesta.data;
+    if(tramites.length>0){
+    	let tramite=tramites[0];
+      	agent.add("El estado de tu trámite es: "+tramite.Estado);
+    } else {
+    agent.add("El número de seguimiento que colocaste no existe ...");
+    }  
   }
-  function ProbandoWebhook(agent) {
-    for (let i = 1; i <= 5; i++) {
-      agent.add(`Esta es la respuesta: ` + i);
-    }
-  }
+  
   let intentMap = new Map();
-  intentMap.set("Default Welcome Intent", welcome);
-  intentMap.set("Default Fallback Intent", fallback);
-  intentMap.set("ProbandoWebhook", ProbandoWebhook);
-  // intentMap.set('your intent name here', yourFunctionHandler);
-  // intentMap.set('your intent name here', googleAssistantHandler);
+  intentMap.set('Tramites.crear', crearTramite);
+  intentMap.set('Tramites.consultar', consultarTramite);
   agent.handleRequest(intentMap);
-});
-
-let port = 3000;
-app.listen(port, () => {
-  console.log("Estamos ejecutando el servidor en el puerto " + port);
 });
